c283c1660e4ff1cbfb932806bfe026c31a070548
shogo takayama <shogo@chalkboard.me>
2021-03-19 14:13:59 +0900

    #9 CSRF対策

---
+++ src/main/java/chalkboard/me/bulletinboard/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package chalkboard.me.bulletinboard.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().permitAll();
+  }
+}
